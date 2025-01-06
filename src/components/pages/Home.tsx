ec47be3587baf2549e089f7d781e6f5a59b802ce
Asher Leffler <asher.leffler@gmail.com>
2025-01-06 12:28:57 -0500

    add backend server and start on sign up proccess

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,23 +1,21 @@
 import Header from "../Header";
 import Footer from "../Footer";
 import "../../css/Home.css";
-import topLeftFire from "../../assets/topLeftFire.svg";
-import topRightFire from "../../assets/bottomRightFire.svg";
 import { Link } from "react-router-dom";
+import bg from "../../assets/background.svg";
 
 export default function Home() {
   return (
     <div className="bodyCont">
+      <img className="homeBackground" src={bg} alt="geometric shapes" />
       <Header />
       <div className="content">
-        <img src={topLeftFire} />
         <div className="title">
           <h1 id="titleText">Sign up and discover the latest trends today</h1>
           <Link to="/signUp">
             Sign Up
           </Link>
         </div>
-        <img src={topRightFire} />
       </div>
       <Footer />
     </div>
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,14 +1,29 @@
+import "../../css/SignUp.css";
+import Header from "../Header";
+import Footer from "../Footer";
+import bg from "../../assets/background.svg";
+
 export default function SignUp() {
-    return (
-        <div>
-            <h1>Sign Up</h1>
-            <form>
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button>Sign Up</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="bodyCont signUp">
+      <Header />
+      <h1>Sign Up</h1>
+      <img className="homeBackground" src={bg} alt="geometric shapes" />
+      <form id="signUpForm">
+        <label htmlFor="userName">User Name</label>
+        <input id="userName" type="text" />
+        <label htmlFor="display">Display Name</label>
+        <input id="display" type="text" />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" placeholder="user@example.com" />
+        <label htmlFor="pass">Password</label>
+        <input id="pass" type="password" />
+        <label htmlFor="confirm">Confirm Password</label>
+        <input id="confirm" type="password" />
+        <button>Sign Up</button>
+        <p className="divider">OR</p>
+      </form>
+      <Footer />
+    </div>
+  );
 }

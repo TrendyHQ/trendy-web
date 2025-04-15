423d864b6be6ec852379b20a846fc13fa7ac442d
Asher Leffler <asher.leffler@gmail.com>
2025-04-15 13:02:22 -0400

    Refactor Footer component to remove isAuthenticated check and simplify loading state handling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,31 @@ import "../css/Footer.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Footer() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isLoading } = useAuth0();
 
   if (isLoading) {
-    return (
-      <footer>
-        <p>&copy; 2025</p>
-      </footer>
-    );
+    return <footer></footer>;
   }
 
   return (
-    <>
-      {isAuthenticated && (
-        <footer>
-          <p>&copy; 2025</p>
-        </footer>
-      )}
-    </>
+    <footer className="bg-[#353447] text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 T. All rights reserved.</p>
+        <p>
+          Follow us on{" "}
+          <a href="#" className="text-blue-400">
+            Twitter
+          </a>{" "}
+          |{" "}
+          <a href="#" className="text-blue-400">
+            Facebook
+          </a>{" "}
+          |{" "}
+          <a href="#" className="text-blue-400">
+            Instagram
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 }
